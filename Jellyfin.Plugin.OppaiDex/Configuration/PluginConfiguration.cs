ff345f47485ad6ecf58df2ab7a6bf5ba18ce3a14
Nana-kaka <115095550+nana-kaka@users.noreply.github.com>
2026-06-08 17:45:35 +0200

    remove Template

--- Jellyfin.Plugin.OppaiDex/Configuration/PluginConfiguration.cs
+++ Jellyfin.Plugin.OppaiDex/Configuration/PluginConfiguration.cs
@@ -1,6 +1,6 @@
 using MediaBrowser.Model.Plugins;
 
-namespace Jellyfin.Plugin.Template.Configuration;
+namespace Jellyfin.Plugin.OppaiDex.Configuration;
 
 /// <summary>
 /// The configuration options.
--- Jellyfin.Plugin.OppaiDex/Plugin.cs
+++ Jellyfin.Plugin.OppaiDex/Plugin.cs
@@ -1,13 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
-using Jellyfin.Plugin.Template.Configuration;
+using Jellyfin.Plugin.OppaiDex.Configuration;
 using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
 
-namespace Jellyfin.Plugin.Template;
+namespace Jellyfin.Plugin.OppaiDex;
 
 /// <summary>
 /// The main plugin.
@@ -26,7 +26,7 @@ public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
     }
 
     /// <inheritdoc />
-    public override string Name => "Template";
+    public override string Name => "OppaiDex";
 
     /// <inheritdoc />
     public override Guid Id => Guid.Parse("eb5d7894-8eef-4b36-aa6f-5d124e828ce1");
